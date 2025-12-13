fe3a8b1e5c90582a97eb4282ad440dad42b94989
nkujyw <1819365448@qq.com>
2025-12-13 10:38:44 +0800

    前端首页初步设计

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -1,14 +1,7 @@
 <?php
-
-/* @var $this \yii\web\View */
-/* @var $content string */
-
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
+use yii\helpers\Url;
 use frontend\assets\AppAsset;
-use common\widgets\Alert;
 
 AppAsset::register($this);
 ?>
@@ -21,62 +14,65 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="icon" href="images/fevicon.png" type="image/gif" />
+    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+    <link rel="stylesheet" href="https://cdn.bootcdn.net/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="https://cdn.bootcdn.net/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
     <?php $this->head() ?>
 </head>
 <body>
 <?php $this->beginBody() ?>
 
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
-    }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $menuItems,
-    ]);
-    NavBar::end();
-    ?>
-
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
+  <div class="header_section padding_bottom_0">
+    <div class="container-fluid">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="logo"><a href="<?= Url::to(['site/index']) ?>"><img src="images/logo.png"></a></div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="<?= Url::to(['site/index']) ?>">首页</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="<?= Url::to(['event/index']) ?>">抗战时间轴</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="<?= Url::to(['character/index']) ?>">英雄与部队</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="<?= Url::to(['memorial/index']) ?>">纪念馆</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="<?= Url::to(['interaction/index']) ?>">互动中心</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
-</div>
+  </div>
+  <?= $content ?>
 
-<footer class="footer">
+  <div class="footer_section layout_padding">
     <div class="container">
-        <p class="pull-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
+      <div class="copyright_text">Copyright 2025 抗战80周年项目组</div>
     </div>
-</footer>
-
+  </div>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="js/jquery-3.0.0.min.js"></script>
+  <script src="js/plugin.js"></script>
+  <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+  <script src="js/custom.js"></script>
+  <script src="js/owl.carousel.js"></script>
+  <script src="https://cdn.bootcdn.net/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
 <?php $this->endBody() ?>
 </body>
 </html>
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,53 +1,77 @@
 <?php
-
-/* @var $this yii\web\View */
-
-$this->title = 'My Yii Application';
+$this->title = '抗战80周年纪念 - 首页';
 ?>
-<div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+<div class="banner_section layout_padding">
+    <div class="container">
+      <div id="main_slider" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <h1 class="banner_taital">铭记历史 <br>吾辈自强</h1>
+            <p class="banner_text">纪念中国人民抗日战争暨世界反法西斯战争胜利80周年...</p>
+            <div class="contact_bt"><a href="#">了解更多</a></div>
+          </div>
+          <div class="carousel-item">
+            <h1 class="banner_taital">英雄不朽 <br>浩气长存</h1>
+            <p class="banner_text">缅怀先烈，珍爱和平...</p>
+            <div class="contact_bt"><a href="#">查看英雄谱</a></div>
+          </div>
         </div>
+        <a class="carousel-control-prev" href="#main_slider" role="button" data-slide="prev">
+          <i class="fa fa-angle-left"></i>
+        </a>
+        <a class="carousel-control-next" href="#main_slider" role="button" data-slide="next">
+          <i class="fa fa-angle-right"></i>
+        </a>
+      </div>
+    </div>
+</div>
 
+<div class="services_section layout_padding">
+    <div class="container">
+      <h1 class="services_taital">重大战役</h1>
+      <p class="services_text">回顾正面战场与敌后战场的关键时刻</p>
+      <div class="services_section_2 layout_padding">
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="box_main">
+              <div class="icon_1"></div> <h6 class="heavy_text">淞沪会战</h6>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="box_main active">
+              <div class="icon_2"></div>
+              <h6 class="heavy_text active">台儿庄大捷</h6>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="box_main">
+              <div class="icon_3"></div>
+              <h6 class="heavy_text">百团大战</h6>
+            </div>
+          </div>
+        </div>
+        <div class="read_bt"><a href="#">查看所有战役</a></div>
+      </div>
+    </div>
+</div>
+
+<div class="trainer_section layout_padding">
+    <div class="container">
+      <h1 class="services_taital">抗战英雄</h1>
+      <p class="services_text">一寸山河一寸血</p>
+      <div class="trainer_section_2 layout_padding">
+        <div class="row">
+          <div class="col-md-6 padding_0">
+            <div class="trainer_box">
+              <h3 class="trainer_text">张自忠</h3>
+              <p class="lorem_text">尽忠报国...</p>
+            </div>
+          </div>
+          <div class="col-md-6 padding_0">
+            <div class="image_1"><img src="images/zhangzizhong.jpg" style="width:100%"></div> 
+          </div>
+        </div>
+      </div>
     </div>
 </div>
