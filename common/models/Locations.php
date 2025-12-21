2e41b4735d00604611eea0dbcf83085e00e72d8f
KK467 <106748927+KK19212575@users.noreply.github.com>
2025-12-21 21:04:06 +0800

    Update Locations.php

--- common/models/Locations.php
+++ common/models/Locations.php
@@ -1,14 +1,14 @@
 
 <?php
+
+namespace common\models;
+
+use Yii;
 /**
 *Team：方圆双睿
 *Coding by 滕一睿 2313109，20251210
 *地点表汉化属性/隐藏id
 */
-namespace common\models;
-
-use Yii;
-
 /**
  * This is the model class for table "locations".
  *
